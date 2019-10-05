d9dbcd7a629f3e89a9e05c21776e7708c70c1c80
Thrishul Lakshmi <thrishulkl13@gmail.com>
2019-10-05 11:59:39 -0400

    My Project Setup Completed Successfully

---
+++ shoppingbackendlk/src/main/java/net/lk/shoppingbackendlk/App.java
@@ -0,0 +1,13 @@
+package net.lk.shoppingbackendlk;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
