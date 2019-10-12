298501ea6467549da46ead04a842ef6ea3a9172f
Thrishul Lakshmi <thrishulkl13@gmail.com>
2019-10-12 08:54:34 -0400

    Completed the interface theme and other issues!

--- onlineshoppinglk/src/main/java/net/lk/onlineshoppinglk/controller/PageController.java
+++ onlineshoppinglk/src/main/java/net/lk/onlineshoppinglk/controller/PageController.java
@@ -1,9 +1,7 @@
 package net.lk.onlineshoppinglk.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -11,11 +9,26 @@ public class PageController {
    
 	@RequestMapping(value = {"/", "/home", "/index"})
 	public ModelAndView index() {
-		
 		ModelAndView mv = new ModelAndView("page");
-		mv.addObject("greeting","Welcome to Spring Web MVC");
-		return mv;
+		mv.addObject("title","Home");
+		mv.addObject("userClickHome",true);
+        return mv;
+	} 
+	
+	@RequestMapping(value = "/about")
+	public ModelAndView about() {
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("title","About Us");
+		mv.addObject("userClickAbout",true);
+        return mv;
 	}
 	
+	@RequestMapping(value = "/contact")
+	public ModelAndView contact() {
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("title","Contact Us");
+		mv.addObject("userClickContact",true);
+        return mv;
+	}
 	
 }
